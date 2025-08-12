9644ba2f3b14631e6ef35a34e96c64ab3f520978
wobsoriano <sorianorobertc@gmail.com>
2025-08-12 10:06:03 -0700

    docs: initial example codes

--- docs/src/components/EmbeddedCheckoutExample.vue
+++ docs/src/components/EmbeddedCheckoutExample.vue
@@ -1,21 +1,18 @@
 <script setup lang="ts">
 import { loadStripe } from '@stripe/stripe-js'
-import { onMounted, ref } from 'vue'
 import { EmbeddedCheckout, EmbeddedCheckoutProvider } from 'vue-stripe'
 
 const stripePromise = loadStripe(import.meta.env.PUBLIC_STRIPE_PUBLIC_KEY as string)
 
-const clientSecret = ref<string>('')
-
-onMounted(async () => {
+async function fetchClientSecret() {
   const resp = await fetch('/api/embedded-checkout')
   const data = await resp.json() as { clientSecret: string }
-  clientSecret.value = data.clientSecret
-})
+  return data.clientSecret
+}
 </script>
 
 <template>
-  <EmbeddedCheckoutProvider :stripe="stripePromise" :options="{ clientSecret }">
+  <EmbeddedCheckoutProvider :stripe="stripePromise" :options="{ fetchClientSecret }">
     <EmbeddedCheckout />
   </EmbeddedCheckoutProvider>
 </template>
