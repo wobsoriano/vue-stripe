c541f998b5e835fc92461ed3171dddf76b75eb46
wobsoriano <sorianorobertc@gmail.com>
2025-08-11 19:31:24 -0700

    doc additions

---
+++ docs/app.config.ts
@@ -0,0 +1,7 @@
+export default defineAppConfig({
+  github: {
+    url: 'https://github.com/wobsoriano/vue-stripe',
+    branch: 'main',
+    rootDir: 'docs'
+  }
+})
--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  site: {
+    name: 'vue-stripe',
+  },
   devtools: { enabled: false },
   imports: {
     autoImport: true,
