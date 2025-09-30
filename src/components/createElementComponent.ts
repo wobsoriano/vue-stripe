e9a1bae43aebffc5f2cc09241f8c7ecdcc2dac90
wobsoriano <sorianorobertc@gmail.com>
2025-09-30 14:37:21 -0700

    chore: Add SPM events

--- src/components/createElementComponent.ts
+++ src/components/createElementComponent.ts
@@ -103,6 +103,8 @@ export function createElementComponent<ElementProps extends Props, ElementEmits 
     useAttachEvent(elementRef, 'shippingaddresschange', emit, Boolean(attrs.onShippingaddresschange))
     useAttachEvent(elementRef, 'shippingratechange', emit, Boolean(attrs.onShippingratechange))
     useAttachEvent(elementRef, 'change', emit, Boolean(attrs.onChange))
+    useAttachEvent(elementRef, 'savedpaymentmethodremove', emit, Boolean(attrs.onSavedPaymentMethodRemove))
+    useAttachEvent(elementRef, 'savedpaymentmethodupdate', emit, Boolean(attrs.onSavedPaymentMethodUpdate))
 
     const shouldEmitElement = type !== 'expressCheckout'
     useAttachEvent(elementRef, 'ready', emit, Boolean(attrs.onReady), shouldEmitElement)
