4182275a724eea686a999f199485ce4982520808
wobsoriano <sorianorobertc@gmail.com>
2025-12-04 11:24:04 -0800

    docs: add sitemap and robots nuxt module

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,6 +1,11 @@
 export default defineNuxtConfig({
   extends: ['docus'],
+  modules: [
+    '@nuxtjs/sitemap',
+    '@nuxtjs/robots',
+  ],
   site: {
     name: 'Vue Stripe',
+    url: 'https://vue-stripe.dev',
   },
 })
